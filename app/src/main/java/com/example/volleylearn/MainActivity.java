382b53d73b5904b45855471ebb87838bfaf8bff4
bearyang <bearyang@pptv.com>
2016-10-17 18:31:52 +0800

    add simple request

--- app/src/main/java/com/example/volleylearn/MainActivity.java
+++ app/src/main/java/com/example/volleylearn/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.volleylearn;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.volleylearn.activity.SimpleRequestActivity;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        Button btnSimple = (Button) findViewById(R.id.btn_simple_request);
+
+        btnSimple.setOnClickListener(btnClickListener);
+    }
+
+    private View.OnClickListener btnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_simple_request:
+                    startActivity(SimpleRequestActivity.class);
+                default:
+                    break;
+            }
+
+        }
+    };
+
+    private void startActivity(Class<?> cls) {
+        Intent intent = new Intent(MainActivity.this, cls);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/volleylearn/activity/SimpleRequestActivity.java
@@ -0,0 +1,111 @@
+package com.example.volleylearn.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.volleylearn.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleRequestActivity extends Activity {
+
+    private static final String TAG = SimpleRequestActivity.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+    private static final String TEST_URL = "http://api.androidhive.info/volley/person_object.json";
+
+    private TextView txtDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_request);
+        initViews();
+
+        // Instantiate the RequestQueue.
+        mRequestQueue = Volley.newRequestQueue(this);
+//        simpleRequest();
+        simplePostRequest();
+    }
+
+    private void initViews() {
+        txtDisplay = (TextView) findViewById(R.id.txt_display);
+    }
+
+    private void simpleRequest() {
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                TEST_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "onResponse, response: " + response);
+                        txtDisplay.setText("Response is: " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "onErrorResponse, error: " + error.getMessage());
+                        txtDisplay.setText("That didn't work!");
+                    }
+                });
+        // Set the tag on the request.
+        stringRequest.setTag(TAG);
+
+        mRequestQueue.add(stringRequest);
+    }
+
+    private void simplePostRequest() {
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                TEST_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "post, onResponse, response: " + response);
+                        txtDisplay.setText("Response is: " + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "post, onErrorResponse, error: " + error.getMessage());
+                        txtDisplay.setText("That didn't work!");
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", "Androidhive");
+                params.put("email", "abc@androidhive.info");
+                params.put("password", "password123");
+
+                return params;
+            }
+        };
+        stringRequest.setTag(TAG);
+        mRequestQueue.add(stringRequest);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(TAG);
+        }
+    }
+}
